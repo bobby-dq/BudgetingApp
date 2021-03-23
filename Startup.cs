9045d7404c5934d89bfbb6f222ffb71cdbf2a9ba
bobby-dq <josh.quilacio@gmail.com>
2021-03-22 21:36:27 -0600

    added essential packages.

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace BudgetingApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,11 +26,13 @@ namespace BudgetingApp
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
 
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
+
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
