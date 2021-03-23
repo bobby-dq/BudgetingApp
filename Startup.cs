fb2c0290d6b57a8864f029632bf3a33b1daa8890
bobby-dq <josh.quilacio@gmail.com>
2021-03-22 21:56:42 -0600

    added tailwind files.

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace BudgetingApp
 {
@@ -14,6 +17,11 @@ namespace BudgetingApp
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration {get; set;}
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -32,11 +40,8 @@ namespace BudgetingApp
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
-
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
