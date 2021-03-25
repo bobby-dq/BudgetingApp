a8d84bce1a0badd7040029b602a1a3f836be5ce7
bobby-dq <josh.quilacio@gmail.com>
2021-03-24 18:47:00 -0600

    Created the income category model.

--- Models/BudgetingModels/IncomeCategory.cs
+++ Models/BudgetingModels/IncomeCategory.cs
@@ -1,7 +1,39 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+
 namespace BudgetingApp.Models.BudgetingModels
 {
     public class IncomeCategory
     {
+        public long IncomeCategoryId {get; set;}
+
+
+        public string UserId {get; set;}
+
+
+        [Required(ErrorMessage="Please enter a description for your income category.")]
+        [StringLength(64, ErrorMessage="The field {0} cannot exceed more than 64 characters.")]
+        public string Description {get; set;}
         
+        
+        [Column(TypeName="decimal(12,2)")]
+        [Display(Name="Budgeted Amount", Prompt="Enter 0 if not known.")]
+        [Required(ErrorMessage="Please enter a budgeted amount. Enter 0 if not known.")]
+        public decimal BudgetedAmount {get; set;}
+
+
+        [Display(Name="Expected Date", Prompt="Leave blank if not known.")]
+        public DateTime? ExpectedDate {get; set;}
+
+
+        public readonly DateTime DateCreated = DateTime.Now;
+
+
+        // Foreign Relationships
+        public long BudgetId {get; set;}
+        public Budget Budget {get; set;}
+        public IEnumerable<IncomeItem> IncomeItems {get;set;}
     }
 }
