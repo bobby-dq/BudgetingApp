4a54d815ba97361f708d667e373977946d27451e
bobby-dq <josh.quilacio@gmail.com>
2021-04-07 19:44:34 -0600

    Created the income category view models and factories.

--- Models/ViewModelFactories/IncomeCategoryFactory.cs
+++ Models/ViewModelFactories/IncomeCategoryFactory.cs
@@ -1,7 +1,75 @@
+using System.Linq;
+using BudgetingApp.Models.BudgetingModels;
+using BudgetingApp.Models.ViewModels;
+
 namespace BudgetingApp.Models.ViewModelFactories
 {
-    public class IncomeCategoryFactory
+    public static class IncomeCategoryFactory
     {
-        
+        public static IncomeCategoryCrudViewModel Create (Budget budget, IncomeCategory incomeCategory)
+        {
+            return new IncomeCategoryCrudViewModel
+            {
+                Budget = budget,
+                IncomeCategory = incomeCategory,
+                Action="Create",
+                ReadOnly = false,
+                ShowAction = true,
+                ActionTheme = "bg-green-200",
+                ButtonTheme = "bg-green-200 hover:bg-green-300"
+            };
+        }
+
+        public static IncomeCategoryCrudViewModel Details (Budget budget, IncomeCategory incomeCategory)
+        {
+            return new IncomeCategoryCrudViewModel
+            {
+                Budget = budget,
+                IncomeCategory = incomeCategory,
+                Action = "Details",
+                ReadOnly = true,
+                ShowAction = false,
+                ActionTheme = "",
+                ButtonTheme = ""
+            };
+        }
+
+        public static IncomeCategoryCrudViewModel Edit (Budget budget, IncomeCategory incomeCategory)
+        {
+            return new IncomeCategoryCrudViewModel
+            {
+                Budget = budget,
+                IncomeCategory = incomeCategory,
+                Action = "Edit",
+                ReadOnly = false,
+                ShowAction = true,
+                ActionTheme = "bg-yellow-200",
+                ButtonTheme = "bg-yellow-200 hover:bg-yellow-300"
+            };
+        }
+
+        public static IncomeCategoryCrudViewModel Delete (Budget budget, IncomeCategory incomeCategory)
+        {
+            return new IncomeCategoryCrudViewModel
+            {
+                Budget = budget,
+                IncomeCategory = incomeCategory, 
+                Action = "Delete",
+                ReadOnly = true,
+                ShowAction = true,
+                ActionTheme = "bg-red-200",
+                ButtonTheme = "bg-red-200 hover:bg-red-300"
+            };
+        }
+
+        public static IncomeCategoryBreakdownViewModel Breakdown (Budget budget, IncomeCategory incomeCategory, IQueryable<IncomeItem> incomeItems)
+        {
+            return new IncomeCategoryBreakdownViewModel
+            {
+                Budget = budget,
+                IncomeCategory = incomeCategory,
+                IncomeItems = incomeItems
+            };
+        }
     }
 }
