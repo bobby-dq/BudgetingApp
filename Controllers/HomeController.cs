4ef4c54cbcbc1a60c7bd54f17f28b9e8cf628c1b
bobby-dq <josh.quilacio@gmail.com>
2021-04-18 12:31:19 -0600

    added the front page.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,20 +11,9 @@ namespace BudgetingApp.Controllers
     [AutoValidateAntiforgeryToken]
     public class HomeController:Controller
     {
-        private BudgetingContext context;
-        public HomeController(BudgetingContext ctx)
-        {
-            context = ctx;
-        }
-
-
         public IActionResult Index()
         {
-            IEnumerable<Budget> budgets = context.Budgets
-                .Include(b => b.ExpenseCategories)
-                .Include(b => b.IncomeCategories);
-            
-            return View("Index", budgets);
+            return View("Index");
         }
     }
 }
