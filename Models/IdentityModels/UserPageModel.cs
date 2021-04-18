8bdaea279f998a8155dd93152947494505207a8b
bobby-dq <josh.quilacio@gmail.com>
2021-04-18 01:10:00 -0600

    added authorization in the account page models.

--- Models/IdentityModels/UserPageModel.cs
+++ Models/IdentityModels/UserPageModel.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BudgetingApp.Models.IdentityModels
 {
+    [Authorize]
     public class UserPageModel:PageModel
     {
         
